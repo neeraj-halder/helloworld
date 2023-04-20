e1ec412960aea050937bb432f745b8b2e354869b
neeraj-halder <neeraj-halder@users.noreply.github.com>
2023-04-20 19:20:57 +0530

    modified helloworld file

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 class HelloWorld{
 
     public static void main(String[] args) {
-        System.out.println(" Hello Neeraj Halder Are you ok?!!");
+        System.out.println(" Hello Neeraj Halder Are you ok? it is triggering automatically!!");
     }
 
 }
